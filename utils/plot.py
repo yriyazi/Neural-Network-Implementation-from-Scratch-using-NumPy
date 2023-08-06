08c3e77100b4842ef96de26aef059dba449570e4
y Riyazi <119464932+yriyazi@users.noreply.github.com>
2023-08-06 17:40:12 +0330

    Update plot.py

--- utils/plot.py
+++ utils/plot.py
@@ -87,7 +87,6 @@ def result_plot(model_name:str,
     plt.savefig(model_name+'.png', bbox_inches='tight')
     plt.show()
     
-# https://www.binarystudy.com/2021/09/how-to-load-preprocess-visualize-CIFAR-10-and-CIFAR-100.html
 def random_plotter( datas,
                     labels,
                     meta,
@@ -96,8 +95,25 @@ def random_plotter( datas,
                     DPI=200,
                     row=10,column=10,
                     pic_in_class=5000
-                    ):
-    
+                    )->None:
+    """
+    Generate a 10x10 grid of random images from different classes of the CIFAR-10 dataset.
+
+    Args:
+        datas (numpy.ndarray): The image data.
+        labels (numpy.ndarray): The labels corresponding to the images.
+        meta (list): List of class names.
+        name (str, optional): Name of the output image file. Defaults to False.
+        figure_size (int, optional): Size of the generated figure. Defaults to 28.
+        DPI (int, optional): Dots per inch for image resolution. Defaults to 200.
+        row (int, optional): Number of rows in the grid. Defaults to 10.
+        column (int, optional): Number of columns in the grid. Defaults to 10.
+        pic_in_class (int, optional): Number of pictures available per class. Defaults to 5000.
+    with help from : https://www.binarystudy.com/2021/09/how-to-load-preprocess-visualize-CIFAR-10-and-CIFAR-100.html
+
+    Returns:
+        None
+    """
     plt.figure(figsize=(figure_size, figure_size), dpi=DPI)
 
     for i in range(row):
@@ -115,15 +131,24 @@ def random_plotter( datas,
         plt.savefig(name+'.png', bbox_inches='tight')
     plt.show()
 
-# https://stackoverflow.com/questions/2148543/how-to-write-a-confusion-matrix
 def compute_confusion_matrix(true, pred):
-  '''Computes a confusion matrix using numpy for two np.arrays
-  true and pred.
+  '''
+  Computes a confusion matrix using NumPy for two numpy arrays: true and pred.
+  
+  Args:
+    true (numpy.ndarray): Array of true labels.
+    pred (numpy.ndarray): Array of predicted labels.
+  
+  Returns:
+    numpy.ndarray: The confusion matrix where rows represent true classes and columns represent predicted classes.
+  with help from : https://stackoverflow.com/questions/2148543/how-to-write-a-confusion-matrix
 
-  Results are identical (and similar in computation time) to: 
-    "from sklearn.metrics import confusion_matrix"
-
-  However, this function avoids the dependency on sklearn.'''
+  
+  This function calculates a confusion matrix to evaluate the performance of a classification model.
+  It counts the occurrences of true positive, true negative, false positive, and false negative predictions for each class.
+  The result is a matrix that provides insights into the model's classification accuracy and misclassifications across different classes.
+  This implementation is independent of the scikit-learn library, providing an alternative way to compute confusion matrices.
+  '''
 
   K = len(np.unique(true)) # Number of classes 
   result = np.zeros((K, K))
@@ -133,8 +158,11 @@ def compute_confusion_matrix(true, pred):
 
   return result
 
-# https://stackoverflow.com/questions/2148543/how-to-write-a-confusion-matrix
-def plot_confusion_matrix(model_name,df_confusion, title='Confusion matrix', cmap=plt.cm.gray_r):
+
+def plot_confusion_matrix(model_name,df_confusion, title='Confusion matrix', cmap=plt.cm.gray_r)->None:
+    '''
+    with help from : https://stackoverflow.com/questions/2148543/how-to-write-a-confusion-matrix
+    '''
     plt.matshow(df_confusion, cmap=cmap) # imshow
     plt.colorbar()
     plt.title(title)
